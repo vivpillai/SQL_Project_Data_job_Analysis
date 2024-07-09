0c581b7a9fce1858e3b3b2598c5a8ebff461fbc4
vivpillai <vivpillai84@gmail.com>
2024-07-08 21:51:19 -0400

    Add files via upload

---
+++ 1.top_paying_job.sql
@@ -0,0 +1,16 @@
+SELECT
+        
+        job_postings_fact
+    WHERE
+        job_title_short = 'Data Analyst' AND
+        job_location = 'Anywhere' AND
+        salary_year_avg IS NOT NULL
+    ORDER BY
+        salary_year_avg DESC
+    LIMIT 10;job_id,
+        job_title,                   
+        job_location,
+        job_schedule_type,
+        salary_year_avg,
+        job_posted_date
+    FROM
---
+++ SELECT.sql
@@ -0,0 +1,15 @@
+SELECT 
+    skills,
+    count(skills_job_dim.job_id) AS job_count
+FROM
+    job_postings_fact
+INNER JOIN skills_job_dim on job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim on skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst' AND 
+    job_work_from_home = 'True'
+GROUP BY
+    skills
+ORDER BY
+    job_count DESC
+LIMIT 5
